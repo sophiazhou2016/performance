2d8148ef1fe4f5d3edfbbcb320709276c1eea7aa
zhourongjing <zhourongjing@ppdai.com>
2020-10-06 17:54:33 +0800

    vue 3.0初体验之setup，1.ref；2.reactive

--- perf-demo/src/App.vue
+++ perf-demo/src/App.vue
@@ -1,25 +1,41 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        <img height="200" :src="item.image" alt="">
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 import { reactive } from 'vue';
 export default {
   name: 'App',
   setup() {
+    const host = 'https://mauloan.ppdaicdn.com/pro';
     const items = reactive([
       {
-        image: ''
+        image: `${host}/202009/371137504268386305.png`
+      },
+      {
+        image: `${host}/202009/371137603052634113.png`
+      },
+      {
+        image: `${host}/202009/371141803531894785.png`
+      },
+      {
+        image: `${host}/202009/371142984647835649.png`
       }
     ])
     console.log('items:', items);
+    return {items};
   },
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
