80b07d86b45e958999d3a4b5ae39c7638ad26145
zhourongjing <zhourongjing@ppdai.com>
2020-10-26 18:27:57 +0800

    如何使用Jsx 开发组件，babel-plugins-jsx

---
+++ vue3.0-demo/src/App.tsx
@@ -0,0 +1,27 @@
+import { defineComponent, reactive, ref } from 'vue'
+
+const img = require('./assets/logo.png') // eslint-disable-line
+
+export default defineComponent({
+    setup() {
+        const state = reactive({
+            name: 'Elsa',
+        })
+
+        const numberRef = ref(1)
+
+        setInterval(() => {
+            state.name += '1'
+            numberRef.value += 1
+        }, 1000)
+
+        return () => {
+            const number = numberRef.value // B 位置：想简写下，reactive 或者 ref 变量的变化会触发这个return 函数的执行，重新渲染
+            return <div id="app">
+                <img src={img} alt="Vue logo" />
+                <p>{state.name + number}</p>
+                <h2>哈哈 ，我做了一个jsx</h2>
+            </div>
+        }
+    }
+})
