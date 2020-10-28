4e82e3a2402cf993c825c7a99bae45f601a9787a
zhourongjing <zhourongjing@ppdai.com>
2020-10-28 12:35:00 +0800

    稍微复杂的json-schema

--- vue3.0-demo/schema-tests/test1.js
+++ vue3.0-demo/schema-tests/test1.js
@@ -1,12 +1,35 @@
 const Ajv = require('ajv')
 
 const schema = {
-    type: 'string',
-    minLength: 10
+    type: 'object',
+    properties: {
+        name: {
+            type: 'string',
+            format: 'email'
+        },
+        age: {
+            type: 'number'
+        },
+        pets: {
+            type: 'array',
+            items: {
+                type: 'string'
+            }
+        },
+        isWorker: {
+            type: 'boolean'
+        }
+    },
+    required: ['name', 'age']
 };
 const ajv = new Ajv()
 const validate = ajv.compile(schema)
-const valid = validate('Elsa')
+const valid = validate({
+    name: 'elsa',
+    age: 18,
+    pets: ['mini', 'mani'],
+    isWorker: true
+})
 if(!valid) {
     console.log(validate.errors)
 }
