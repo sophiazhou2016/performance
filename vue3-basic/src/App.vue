021b6f4c5f3a6e41984add53ab9bb75cf5324a25
zhourongjing <zhourongjing@ppdai.com>
2021-02-14 20:51:32 +0800

    如果需要的是对象，则使用reactive，但是需要写很多data. ,优化成return{...data}，但是会失去响应式。 使用toRefs()使其中的每一项都变成响应式的！ 还学习了申明interface DataProps

--- vue3-basic/src/App.vue
+++ vue3-basic/src/App.vue
@@ -3,28 +3,42 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>{{count}}</h1>
     <h1>{{double}}</h1>
-    <button @click="increse">点赞+1</button>
+    <button @click="increase">点赞+1</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed } from 'vue';
-// import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, ref, reactive, toRefs, computed } from 'vue';
 
+interface DataProps {
+  count: number;
+  double: number;
+  increase: () => void;
+}
 export default defineComponent({
   name: 'App',
   setup() {
-    const count = ref(0)
-    const double = computed(() => {
-      return count.value * 2
+    // const count = ref(0)
+    // const double = computed(() => {
+    //   return count.value * 2
+    // })
+    // const increase = () => {
+    //   count.value ++
+    // }
+    const data: DataProps = reactive({
+      count: 0,
+      increase: () => { data.count++ },
+      double: computed(() => data.count * 2)
     })
-    const increse = () => {
-      count.value ++
-    }
+    const refData = toRefs(data)
+    refData.count // 他里面的每一项都会变成响应式的，可以看到是Ref<number>
     return {
-      count,
-      increse,
-      double
+      ...refData
+      // ...data
+      // 把值取出来会变成普通的值，失去响应式
+      // count: data.count,
+      // double: data.double,
+      // increase: data.increase
     }
   }
 });
