718609ccc5c0f2ccc429252f93b0a5049cbbbd8d
zhourongjing <zhourongjing@ppdai.com>
2021-02-19 19:03:51 +0800

    初始化项目zheye

---
+++ README.md
@@ -0,0 +1,3 @@
+# ts-demo 学习 ts 的基础知识
+
+# vue3-basic 学习 vue3 的一些新的 composition api
--- vue3-basic/src/components/DogShow.vue
+++ vue3-basic/src/components/DogShow.vue
@@ -7,7 +7,7 @@ import { defineComponent } from 'vue'
 import axios from 'axios';
 export default defineComponent({
     async setup() {
-        const rawData = await axios.get('https://dog.ceo/api/breeds/image')
+        const rawData = await axios.get('https://dog.ceo/api/breeds/image/random')
         return {
             result: rawData.data
         }
