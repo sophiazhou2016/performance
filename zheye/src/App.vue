d13d1c957c6497f1b178f8126836a6f4a9bc8bd3
zhourongjing <zhourongjing@ppdai.com>
2021-02-19 20:49:39 +0800

    GlobalHeader组件

--- zheye/src/App.vue
+++ zheye/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <global-header :user="currentUser"></global-header>
     <column-list :list="list"></column-list>
   </div>
 </template>
@@ -8,6 +9,11 @@
 import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+const currentUser: UserProps = {
+  isLogin: true,
+  name: 'jingjing'
+}
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -37,11 +43,13 @@ const testData: ColumnProps[] = [
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList
+    ColumnList,
+    GlobalHeader
   },
   setup () {
     return {
-      list: testData
+      list: testData,
+      currentUser
     }
   }
 })
---
+++ zheye/src/components/GlobalHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary justiify-content-between mb-4 px-4">
+    <a class="navbar-brand" href="#">者也专栏</a>
+    <ul v-if="!user.isLogin" class="list-inline mb-0">
+      <li class="list-inline-item">
+        <a class="btn btn-outline-light my-2" href="#">登录</a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn btn-outline-light my-2" href="#">注册</a>
+      </li>
+    </ul>
+    <ul v-else>
+      <li class="list-inline-item">
+        <a class="btn btn-outline-light mys-2" href="#">你好 {{user.name}}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType } from 'vue'
+export interface UserProps {
+  isLogin: boolean;
+  name?: string;
+  id?: string;
+}
+export default defineComponent({
+  name: 'GlobalHeader',
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
